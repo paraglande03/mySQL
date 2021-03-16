f3c1f710ba640516cca5c46cb4bd2dd0fbf9b0bd
paraglande03 <paraglande03@gmail.com>
2021-03-16 12:35:00 +0530

    Added: Script for creating table in DB

--- EmpPayroll.sql
+++ EmpPayroll.sql
@@ -2,3 +2,12 @@ CREATE DATABASE payroll_service;
 use payroll_service;
 SHOW databases;
 SELECT  DATABASE();
+CREATE TABLE employee_payroll
+(
+	id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name		VARCHAR(255) NOT NULL,
+    salary		double NOT NULL,
+    Start_date	DATE NOT NULL,
+    PRIMARY KEY	(id)
+);
+DESCRIBE employee_payroll;
