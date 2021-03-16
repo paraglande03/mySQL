688e62885792e28f1174bb28033fc55da1bdb393
paraglande03 <paraglande03@gmail.com>
2021-03-16 12:50:00 +0530

    Added: Script to see specific row using where clause

--- EmpPayroll.sql
+++ EmpPayroll.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE payroll_service;
+
 use payroll_service;
+
 SHOW databases;
+
 SELECT  DATABASE();
+
 CREATE TABLE employee_payroll
 (
 	id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -10,9 +14,18 @@ CREATE TABLE employee_payroll
     Start_date	DATE NOT NULL,
     PRIMARY KEY	(id)
 );
+
 DESCRIBE employee_payroll;
+
 INSERT INTO employee_payroll(name , salary, Start_date) VALUES
 ('BILL',1000000,'2018-01-03'),
 ('TERISA',2000000,'2019-11-13'),
 ('CHARLIE',3000000,'2020-05-21');
+
 SELECT * FROM employee_payroll;
+
+SELECT salary FROM employee_payroll
+WHERE name = 'BILL';
+
+SELECT * FROM employee_payroll
+WHERE Start_date BETWEEN CAST('2019-01-03' AS DATE) and DATE (NOW());
